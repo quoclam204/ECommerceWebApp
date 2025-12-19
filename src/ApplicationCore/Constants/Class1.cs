c5ac5f75c924a0f9275ea5f7db378213e23ff4a1
quoclam204 <nguyenlequoclam@gmail.com>
2025-12-19 14:19:04 +0700

    Fix solution paths after restructuring project folders

---
+++ src/ApplicationCore/Constants/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Constants
+{
+    internal class Class1
+    {
+    }
+}
